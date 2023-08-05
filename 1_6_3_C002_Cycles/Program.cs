d3da00ab3b1821e126ac27af8bcab9fefd52901a
Svetlana Panina <paninasi@inbox.ru>
2023-08-05 04:42:29 +0300

    1_6_3_C002_Cycles

---
+++ 1_6_3_C002_Cycles/Program.cs
@@ -0,0 +1,17 @@
+﻿// Цикл "for":
+string str = String.Empty; // = ""
+for (int i = 0; i < 3; i++)
+{
+    str += "Склейка";
+}
+Console.WriteLine(str);
+
+// Вложенный цикл "for":
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.Write($"{i, 2} x {j} ={i * j, 3}  ");
+    }
+    Console.WriteLine();
+}
