6a852f2347c237a58f2cef9c62e912ae7986b4ab
Svetlana Panina <paninasi@inbox.ru>
2023-08-05 04:43:43 +0300

    1_6_3_C003_ChangeText

---
+++ 1_6_3_C003_ChangeText/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пример 1 (1.6.3.)
+// Дан текст. В тексте нужно:
+// – все пробелы заменить черточками,
+// – маленькие буквы "к" заменить большими "К",
+// – а большие "С" заменить маленькими "с".
+
+string text = "– Я думаю, – Сказал князь, улыбаясь, – что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом Согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result += $"{newValue}";
+        else result += $"{text[i]}";
+    }
+    
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+Console.WriteLine();
